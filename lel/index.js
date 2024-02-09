2b2f0f49ec68a43f84cc6c8873dd0e80a6887824
JunniorChinchay <22junnior12345@gmail.com>
2024-02-08 19:28:24 -0500

    nuevo index

---
+++ lel/index.js
@@ -0,0 +1,13 @@
+const fin = () => {
+    const miString = "soy un string"
+    let b = miString + "muy feliz"
+    console.log(b)
+}
+
+fin()
+
+const clickeado = ()=> {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
